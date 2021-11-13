524f733a53aa4a9d4804c12275b316a9e3b11b9e
eddybruv <edwinajong@gmail.com>
2021-11-13 03:59:09 +0100

    description in file

---
+++ Modular Programming/fib.c
@@ -0,0 +1,35 @@
+/**
+ *The nth term of the fibonacci series using recursion.
+ **/
+
+#include <stdio.h>
+
+int fibonacci(int);
+
+int main(void) {
+    //! showMemory(start=65520)
+    int N, fib;
+    printf("Which Fibonacci number would you like: ");
+    scanf("%d", &N);
+    
+    if (N <= 0) {
+        printf("%d is not positive. Aborting!\n", N);
+    }
+    else {
+        fib = fibonacci(N);
+        printf("The %dth Fibonacci number is %d.\n", N, fib);
+    }
+	return 0;
+}
+
+int fibonacci(int n) {
+    if (n == 1) {
+        return 0;
+    } 
+    else if (n == 2) {
+        return 1;
+    } 
+    else {
+        return (fibonacci(n-1) + fibonacci(n-2));
+    }
+}
