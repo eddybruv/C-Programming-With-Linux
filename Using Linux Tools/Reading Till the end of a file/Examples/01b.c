764aa58d896ec59e2d79f13b50ced50ad9063a9d
eddybruv <edwinajong@gmail.com>
2022-01-02 21:26:25 +0100

    better version of 01

---
+++ Using Linux Tools/Reading Till the end of a file/Examples/01b.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE *ifile;
+    int i, num, sum;
+    int N;
+
+    ifile = fopen("files/my_third_file.txt", "r");
+
+    N = 0;
+    sum = 0;
+
+    while (fscanf(ifile, "%d", &num) != EOF)
+    {
+        printf("I read %d from the file. \n", num);
+        sum += num;
+        N++;
+    }
+
+    fclose(ifile);
+
+    printf("There are %d numbers in the file.\n", N);
+    printf("The sum of the numbers read is %d.\n", sum);
+    printf("The average is %.2f.", (float)sum / N);
+
+    return (0);
+}
