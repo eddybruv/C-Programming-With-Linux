787ad4d83c9068fb687f52ad6e877d07f6b0cabf
eddybruv <edwinajong@gmail.com>
2022-01-03 05:10:30 +0100

    writing to a file using fprintf()

---
+++ Using Linux Tools/Writing to a file/Example/01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    FILE *fp;
+    char filename[] = "file.txt";
+    int num;
+
+    fp = fopen(filename, "w");
+
+    do {
+        printf("Please enter a grade. Enter -1 to quit: ");
+        scanf("%d", num);
+        
+        if(num != -1)
+            fprintf(fp, "%d", num);
+    } while (num != -1);
+    
+    fclose(fp);
+    return 0;
+}
